84ea3f661bb3522e3dc23a782b7d61e49076e5c4
ZY Deng <me@zydeng.com>
2024-04-14 19:11:52 +0800

    fix content-type check

--- source.go
+++ source.go
@@ -15,8 +15,9 @@ func URLSource(url string) (io.Reader, error) {
 	if res.StatusCode != 200 {
 		return nil, fmt.Errorf("Invalid HTTP response: %s", res.Status)
 	}
-	if res.Header["Content-Type"][0] != "application/ogg" {
-		return nil, fmt.Errorf("Wrong MIME type (%s), are you sure this is an ogg/vorbis stream?", res.Header["Content-Type"])
+	mime := res.Header["Content-Type"][0]
+	if mime != "application/ogg" && mime != "audio/ogg" {
+		return nil, fmt.Errorf("Wrong MIME type (%s), are you sure this is an ogg/vorbis stream?", mime)
 	}
 
 	return res.Body, nil
